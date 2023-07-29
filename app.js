a1aec9fac10e5dba4e7a950f83e9200f33e67888
azevedowski <111291446+azevedowski@users.noreply.github.com>
2023-07-29 18:12:37 -0300

    Enviando informações

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("teste")
 console.log("o Paulo fez essa alteração sem você ver na aula")
+console.log("enviando para o GitHub")
+
