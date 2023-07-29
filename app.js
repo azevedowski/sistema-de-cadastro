e452b1545f86ac6a15be802b74de99dbf2eb1e31
azevedowski <111291446+azevedowski@users.noreply.github.com>
2023-07-29 18:26:28 -0300

    nova munança

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("teste")
 console.log("o Paulo fez essa alteração sem você ver na aula")
-console.log("enviando para o GitHub")
-
